20dcd76a8383481c4ed2c8ea1982859796819598
leungashley76 <leungashley76@gmail.com>
2022-10-14 17:45:21 -0400

    testing Math.round

---
+++ src/TestingMath.java
@@ -0,0 +1,7 @@
+public class TestingMath {
+    public static void main(String[] args) {
+        System.out.println(Math.round(6.9));
+        System.out.println(Math.round(6.5));
+        System.out.println(Math.round(6.1));
+    }
+}
